87816116140a01c1a824ae245e5800d6768ed784
norre2k <kevinkvillebo@hotmail.com>
2025-04-01 15:20:10 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDB;
+ class Movie
+{
+    public string Title { get; set; }
+    public int RunningTime { get; set; }
+    public List<Actor> Actors { get; set; }
+
+    public Movie(string title, int runningtime, List<Actor> actors)
+    {
+        Title = title;
+        RunningTime = runningtime;
+        Actors = actors;
+    }
+
+    public virtual void ShowMovieInfo()
+    {
+        Console.Write("The movie " +  Title + " is " + RunningTime + " minutes long. The characters are played by: ");
+        foreach(Actor actor in Actors)
+        {
+            Console.Write(actor.Name + "(born in " + actor.BirthYear + "), ");
+        }
+
+    }
+}
+
+class Actor
+{
+    public string Name { get; set; }
+    public int BirthYear { get; set; }
+
+    public Actor(string name, int birtyear)
+    {
+        Name = name;
+        BirthYear = birtyear;
+    }
+
+}
+
+
+
+
+class Comedy : Movie
+{
+    public int NumberOfFunnyScenes { get; set; }
+
+    public Comedy(string title, int runningtime, List<Actor> actors, int numberoffunnyscenes) : base(title, runningtime, actors)
+    {
+        NumberOfFunnyScenes = numberoffunnyscenes;
+    }
+
+    public override void ShowMovieInfo()
+    {
+        base.ShowMovieInfo();
+        Console.Write(" The movie has " + NumberOfFunnyScenes + " Funny scenes.");
+        Console.WriteLine();
+    }
+}
+
+class Action : Movie
+{
+    public int NumberOfShotsFired { get; set; }
+
+    public Action(string title, int runningtime, List<Actor> actors, int numberofshotsfired) : base(title, runningtime, actors)
+    {
+        NumberOfShotsFired = numberofshotsfired;
+    }
+
+    public override void ShowMovieInfo()
+    {
+        base.ShowMovieInfo();
+        Console.Write(" The movie has " + NumberOfShotsFired + " shots fired.");
+        Console.WriteLine();
+    }
+}
+
+class SciFi : Movie
+{
+    public int NumberOfSpaceShips { get; set; }
+
+    public SciFi(string title, int runningtime, List<Actor> actors, int numberofspaceships) : base(title, runningtime, actors)
+    {
+        NumberOfSpaceShips = numberofspaceships;
+    }
+
+    public override void ShowMovieInfo()
+    {
+        base.ShowMovieInfo();
+        Console.Write(" The movie has " + NumberOfSpaceShips + " space ships.");
+        Console.WriteLine();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MovieDB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>();
+            List<Actor> actors = new List<Actor>();
+            actors.Add(new Actor("Tom Cruise", 1969));
+            actors.Add(new Actor("Kevin Heart", 1979));
+
+           
+            movies.Add(new Comedy("Jumanji", 160, actors, 20));
+            movies.Add(new Action("Top Gun", 175, actors, 400));
+            movies.Add(new SciFi("Oblivion", 210, actors, 7));
+            movies.Add(new SciFi("scooby", 75, actors, 13));
+            movies.Add(new Comedy("King Kong", 140, actors, 2));
+
+            
+                foreach (Movie movie in movies)
+                {
+                if (movie is Comedy)
+                {
+                    movie.ShowMovieInfo();
+                }
+                else if (movie is Action)
+                {
+                    movie.ShowMovieInfo();
+                }
+                else if(movie is SciFi)
+                {
+                    movie.ShowMovieInfo();
+                }
+                    
+                }
+            
+        }
+    }
+}
